1c43ca0efb4016928d4be055bdcc892d748f2b25
Marco Tarchini <71197033+marcotarchini@users.noreply.github.com>
2022-09-27 10:25:12 -0300

    Add files via upload

---
+++ Modulo POO/index.php
@@ -0,0 +1,33 @@
+<script>
+    class password {
+        private let _lengthWord;
+        private let _wordSafe;
+
+        constructor(_lengthWord, wordSafe){
+            $this._lengthWord= _lengthWord;
+            $this.wordSafe= wordSafe;
+        }
+
+        constructor(){
+
+        } 
+        
+        get get_lengthWord(){
+            return this._lengthWord;
+        }
+        get getwordSafe(){
+            return this.wordSafe;
+        }
+        set setwordSafe(wordSafe){
+            this.wordSafe=wordSafe;
+        }
+
+        esFuerte(){
+            
+        }
+        generarPassword(){
+            let passAux= this._lengthWord;
+            let aux = length (passAux);
+        }
+    }
+</script>
